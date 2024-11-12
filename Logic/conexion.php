bdcf47e12d4d90cc1ecc44165ad23674e32f3bbf
aracelilozada <arceliterroneslozada@gmail.com>
2024-11-11 22:58:49 -0500

    sistemas de proyecto final

---
+++ Logic/conexion.php
@@ -0,0 +1,13 @@
+<?php
+//Definimos las variables de conexion
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "sistemas-de-pagos";
+//Creamos la conexion
+try {
+    $conexion = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pass);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error en la conexion con PDO : " . $e->getMessage();
+}
